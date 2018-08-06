22ecb6f7bfcf1e16e0cadce55763fe2b5f488377
Enguerrand des Vaux <enguerrand@des-vaux.fr>
2018-08-06 13:35:17 +1000

    improve sample

--- arcgis.config.js
+++ arcgis.config.js
@@ -3,6 +3,5 @@ const libFolder = path.join(process.cwd(), "src");
 
 module.exports = {
   outputPath: path.join(process.cwd(), "public"),
-  libEntry: path.join(libFolder, "arcgis.js"),
-  dojoModules: ["esri/Map", "esri/views/MapView", "esri/views/SceneView"]
+  dojoModules: ["esri/Map", "esri/views/SceneView", "esri/layers/SceneLayer"]
 };
